3d93a510bb1682b1ff6f0a95f90526d4d1fb75f8
Yoon Kim <imyoonkim@gmail.com>
2021-04-01 23:34:29 -0700

    add hover effects

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,15 @@
 // AOS
 AOS.init();
+
+
+// Navbar Scroll effect 
+window.addEventListener("scroll", function() {
+    let navbar = document.getElementsByTagName('nav')[0];
+    
+    if(window.pageYOffset > 0) {
+      navbar.classList.add('scroll-nav');
+      navbar.style.transition= "all 0.1s";
+    }else {
+      navbar.classList.remove("scroll-nav");
+    }
+  });
