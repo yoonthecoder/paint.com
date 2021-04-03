be319a5a3b7fb4330795d6a0c056d7ad68ebac8e
Yoon Kim <imyoonkim@gmail.com>
2021-04-03 00:06:44 -0700

    add modal boxes

--- dist/js/main.js
+++ dist/js/main.js
@@ -13,3 +13,46 @@ window.addEventListener("scroll", function() {
       navbar.classList.remove("scroll-nav");
     }
   });
+
+  const loginBtn = document.querySelector('.login-btn');
+  const closeBtn = document.querySelector('.close-btn');
+  const loginModal = document.querySelector('.login-modal');
+  const loginSignupBtn = document.querySelector('.login-signup-btn')
+  const signupModal = document.querySelector('.signup-modal');
+  const signupBtn = document.querySelector('.signup-btn');
+  const signupCloseBtn = document.querySelector('.signup-close-btn');
+  
+
+  loginBtn.addEventListener('click', function() {
+    loginModal.style.display = "block";
+      closeBtn.addEventListener('click',function() {
+        loginModal.style.display = "none";
+      })
+      loginSignupBtn.addEventListener('click',function() {
+        loginModal.style.display = "none";     
+        signupModal.style.display = "block";
+      })
+  });
+
+  signupBtn.addEventListener('click',function() {
+    signupModal.style.display= "block";
+    if(signupModal.style.display = "block"){
+      signupCloseBtn.addEventListener('click',function() {
+        signupModal.style.display ="none";
+      });
+    }
+  });
+
+// Mobile Menu
+
+const menu = document.querySelector('.hamburger');
+const nav = document.querySelector('.nav-items');
+
+
+menu.addEventListener('click', () => {
+  nav.classList.toggle('nav-active');
+});
+
+
+
+  
